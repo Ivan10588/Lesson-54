5731a4a71dda8dcacfdb085d477de7d4f3fdb329
Ivan10588 <Ivanov_Ivan_88@mail.ru>
2026-05-31 18:44:18 +1100

    Lesson_54

--- product_parser.py
+++ product_parser.py
@@ -8,3 +8,16 @@ import schedule
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def basic_parser(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup
+    else:
+        print(f"Ошибка: {response.status_code}")
+        return None
+
+
